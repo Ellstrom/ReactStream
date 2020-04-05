4ed987a1049f47aa615d64774baf2a180c8a9115
Jonathan Ellström <jonathan.ellstrom@hotmail.com>
2020-04-05 11:24:03 +0200

    Small fix

--- src/components/ParentYoutubeComponent.js
+++ src/components/ParentYoutubeComponent.js
@@ -3,7 +3,6 @@ import YoutubeComponent from "./YoutubeComponent";
 
 class ParentYoutubeComponent extends React.Component {
     render() {
-
         if(this.props.videoId1 !== "hidden" && this.props.videoId2 !== "hidden"){
             return (
                 <tr>
