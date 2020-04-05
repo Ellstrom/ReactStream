34c3fd1604d5bfd6ca1f9ec3e3c9dd905f39efb8
Jonathan Ellström <jonathan.ellstrom@hotmail.com>
2020-04-05 13:35:03 +0200

    Added more config files

---
+++ src/configurations/OverpassConfig.js
@@ -0,0 +1,127 @@
+export const OverpassConfig = {
+    executes : {
+        execute_1 : {
+            name: "hidden",
+            videoIds : {
+                videoId1: "hidden",
+                videoId2: "hidden",
+                videoId3: "hidden",
+                videoId4: "hidden",
+                videoId5: "hidden",
+                videoId6: "hidden",
+                videoId7: "hidden",
+                videoId8: "hidden",
+                videoId9: "hidden",
+                videoId10: "hidden"
+            },
+        },
+        execute_2 : {
+            name: "hidden",
+            videoIds : {
+                videoId1: "hidden",
+                videoId2: "hidden",
+                videoId3: "hidden",
+                videoId4: "hidden",
+                videoId5: "hidden",
+                videoId6: "hidden",
+                videoId7: "hidden",
+                videoId8: "hidden",
+                videoId9: "hidden",
+                videoId10: "hidden"
+            },
+        },
+        execute_3 : {
+            name: "hidden",
+            videoIds : {
+                videoId1: "hidden",
+                videoId2: "hidden",
+                videoId3: "hidden",
+                videoId4: "hidden",
+                videoId5: "hidden",
+                videoId6: "hidden",
+                videoId7: "hidden",
+                videoId8: "hidden",
+                videoId9: "hidden",
+                videoId10: "hidden"
+            },
+        },
+        execute_4 : {
+            name: "hidden",
+            videoIds : {
+                videoId1: "hidden",
+                videoId2: "hidden",
+                videoId3: "hidden",
+                videoId4: "hidden",
+                videoId5: "hidden",
+                videoId6: "hidden",
+                videoId7: "hidden",
+                videoId8: "hidden",
+                videoId9: "hidden",
+                videoId10: "hidden"
+            },
+        },
+        execute_5 : {
+            name: "hidden",
+            videoIds : {
+                videoId1: "hidden",
+                videoId2: "hidden",
+                videoId3: "hidden",
+                videoId4: "hidden",
+                videoId5: "hidden",
+                videoId6: "hidden",
+                videoId7: "hidden",
+                videoId8: "hidden",
+                videoId9: "hidden",
+                videoId10: "hidden"
+            },
+        },
+    },
+    T : {
+        videoIds : {
+            videoId1: "hidden",
+            videoId2: "hidden",
+            videoId3: "hidden",
+            videoId4: "hidden",
+            videoId5: "hidden",
+            videoId6: "hidden",
+            videoId7: "hidden",
+            videoId8: "hidden",
+            videoId9: "hidden",
+            videoId10: "hidden",
+            videoId11: "hidden",
+            videoId12: "hidden",
+            videoId13: "hidden",
+            videoId14: "hidden",
+            videoId15: "hidden",
+            videoId16: "hidden",
+            videoId17: "hidden",
+            videoId18: "hidden",
+            videoId19: "hidden",
+            videoId20: "hidden"
+        }
+    },
+    CT : {
+        videoIds : {
+            videoId1: "hidden",
+            videoId2: "hidden",
+            videoId3: "hidden",
+            videoId4: "hidden",
+            videoId5: "hidden",
+            videoId6: "hidden",
+            videoId7: "hidden",
+            videoId8: "hidden",
+            videoId9: "hidden",
+            videoId10: "hidden",
+            videoId11: "hidden",
+            videoId12: "hidden",
+            videoId13: "hidden",
+            videoId14: "hidden",
+            videoId15: "hidden",
+            videoId16: "hidden",
+            videoId17: "hidden",
+            videoId18: "hidden",
+            videoId19: "hidden",
+            videoId20: "hidden"
+        }
+    }
+};
