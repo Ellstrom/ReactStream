7ef84b4345ded918b43944b899c95e39c2e607b6
Jonathan Ellström <jonathan.ellstrom@hotmail.com>
2020-05-12 20:39:37 +0200

    Fixed some css for frontpage

--- src/components/ContentGrid.js
+++ src/components/ContentGrid.js
@@ -12,7 +12,7 @@ const ContentGrid = (props) => {
 
     return (
         <div className="grid-container-videos">
-            <YoutubeComponent videoId={props.props.executes.execute_1.videoIds.videoId1}/>
+            <YoutubeComponent hidden={false} videoId={props.props.executes.execute_1.videoIds.videoId1}/>
             <YoutubeComponent videoId={props.props.executes.execute_1.videoIds.videoId2}/>
             <YoutubeComponent videoId={props.props.executes.execute_1.videoIds.videoId1}/>
             <YoutubeComponent videoId={props.props.executes.execute_1.videoIds.videoId2}/>
--- src/components/YoutubeComponent.js
+++ src/components/YoutubeComponent.js
@@ -12,7 +12,7 @@ class YoutubeComponent extends React.Component {
             },
         };
 
-        if(this.props.videoId !== "hidden"){
+        if(!this.props.hidden && this.props.videoId1 !== "hidden"){
             return <YouTube videoId={this.props.videoId} opts={opts} onReady={this._onReady} />;
         }else{
             return null;
