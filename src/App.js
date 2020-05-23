766f502b1b6b2b5a219c1f40191ca628d21b1d8a
Jonathan Ellström <jonathan.ellstrom@hotmail.com>
2020-05-23 16:47:23 +0200

    Base for toggle theme

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import EllstreamNavbar from "./components/EllstreamNavbar";
 import MapPage from "./components/MapPage";
@@ -13,29 +13,49 @@ import OverpassSrc from "./images/overpass/overpass.jpg";
 import TrainSrc from "./images/train/train.jpg";
 import AnubisSrc from "./images/anubis/anubis.jpg";
 import Feedback from "./components/Feedback";
-
+import { ThemeProvider} from "styled-components";
+import { lightTheme, darkTheme} from "./components/theme";
+import { GlobalStyles} from "./components/global";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
+
+    const [theme, setTheme] = useState('light');
+
+    const toggleTheme = () => {
+        if(theme === 'light') {
+            setTheme('dark');
+        }else{
+            setTheme('light');
+        }
+    };
+
     return (
-        <Router>
-            <div className="App">
-                <EllstreamNavbar/>
-                <Switch>
-                    <Route path="/" exact component={HomePage}/>
-                    <Route path="/Mirage" component={() => <MapPage mapName="Mirage" mapSrc={MirageSrc}/>}/>
-                    <Route path="/Inferno" component={() => <MapPage mapName="Inferno" mapSrc={InfernoSrc}/>}/>
-                    <Route path="/Dust2" component={() => <MapPage mapName="Dust2" mapSrc={Dust2Src}/>}/>
-                    <Route path="/Nuke" component={() => <MapPage mapName="Nuke" mapSrc={NukeSrc}/>}/>
-                    <Route path="/Vertigo" component={() => <MapPage mapName="Vertigo" mapSrc={VertigoSrc}/>}/>
-                    <Route path="/Overpass" component={() => <MapPage mapName="Overpass" mapSrc={OverpassSrc}/>}/>
-                    <Route path="/Anubis" component={() => <MapPage mapName="Anubis" mapSrc={AnubisSrc}/>}/>
-                    <Route path="/Cache" component={() => <MapPage mapName="Cache" mapSrc={CacheSrc}/>}/>
-                    <Route path="/Train" component={() => <MapPage mapName="Train" mapSrc={TrainSrc}/>}/>
-                    <Route path="/Feedback" component={Feedback}/>
-                </Switch>
-            </div>
-        </Router>
+        <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+            <Router>
+                <div className="App">
+                    <EllstreamNavbar/>
+                    <>
+                        <GlobalStyles />
+                        <button onClick={toggleTheme}> Toggle theme</button>
+                        <h1>It's a light theme!</h1>
+                    </>
+                    <Switch>
+                        <Route path="/" exact component={HomePage}/>
+                        <Route path="/Mirage" component={() => <MapPage mapName="Mirage" mapSrc={MirageSrc}/>}/>
+                        <Route path="/Inferno" component={() => <MapPage mapName="Inferno" mapSrc={InfernoSrc}/>}/>
+                        <Route path="/Dust2" component={() => <MapPage mapName="Dust2" mapSrc={Dust2Src}/>}/>
+                        <Route path="/Nuke" component={() => <MapPage mapName="Nuke" mapSrc={NukeSrc}/>}/>
+                        <Route path="/Vertigo" component={() => <MapPage mapName="Vertigo" mapSrc={VertigoSrc}/>}/>
+                        <Route path="/Overpass" component={() => <MapPage mapName="Overpass" mapSrc={OverpassSrc}/>}/>
+                        <Route path="/Anubis" component={() => <MapPage mapName="Anubis" mapSrc={AnubisSrc}/>}/>
+                        <Route path="/Cache" component={() => <MapPage mapName="Cache" mapSrc={CacheSrc}/>}/>
+                        <Route path="/Train" component={() => <MapPage mapName="Train" mapSrc={TrainSrc}/>}/>
+                        <Route path="/Feedback" component={Feedback}/>
+                    </Switch>
+                </div>
+            </Router>
+        </ThemeProvider>
     );
 }
 
